483a1f16f9b32ab2b926831b6f20ec97e7218868
Oleh Zahorulko <o.zahorulko@macopedia.com>
2021-07-20 09:54:08 +0200

    an updated commit message

---
+++ my_library/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'My Library',
+    'summary': "Manage books easily",
+    'description': """
+        Manage Library
+        ==============
+        Description related to library.
+    """,
+    'author': "Oleh Zaho",
+    'website': "http://www.example.com",
+    'category': 'Uncategorized',
+    'version': '13.0.77',
+    'depends': ['base'],
+    'data': ['views/views.xml'],
+    'demo': ['demo.xml'],
+ }
